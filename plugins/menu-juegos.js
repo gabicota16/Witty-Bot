61ee5de2690581f65f81db0f55e4d43c2959ad4c
gabicota16 <134754166+gabicota16@users.noreply.github.com>
2023-06-23 20:43:42 -0300

    𝓦𝓲𝓽𝓽𝔂-𝓑𝓸𝓽

--- plugins/menu-juegos.js
+++ plugins/menu-juegos.js
@@ -19,7 +19,7 @@ import fetch from 'node-fetch'
  let str = ` 
  ╭═══✠𝗠̲𝗲̲𝗻̲𝘂̲ 𝗝̲𝘂̲𝗲̲𝗴̲𝗼̲𝘀̲✠═══╮   
  ╿ 
- ╿➪🃏${usedPrefix}buscarpalabra
+ ╿➪🃏${usedPrefix}sopa
  ╿ 
  ╿➪🃏${usedPrefix}slot *cantidad*
  ╿
