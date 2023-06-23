8f0bb516e20169a6afa110db564249b9e10f0f50
gabicota16 <134754166+gabicota16@users.noreply.github.com>
2023-06-23 15:34:26 -0300

    𝓦𝓲𝓽𝓽𝔂-𝓑𝓸𝓽

--- config.js
+++ config.js
@@ -176,6 +176,7 @@ global.imagen12 = fs.readFileSync('./media/menus/img11.jpg')
 global.imagen13 = fs.readFileSync('./media/menus/img12.jpg')
 global.imagen14 = fs.readFileSync('./media/menus/menustickers.jpeg')
 global.imagen15 = fs.readFileSync('./media/menus/menujuegos.jpg')
+global.imagen15 = fs.readFileSync('./media/menus/menudescargas.jpeg')
 
 //━━━━━━━━━━━━━━━━━━━━ 
 
